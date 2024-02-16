2532a77598894fe9466e3fd7d8e22272f89f0491
신희성 <vvwv3092@gmail.com>
2024-02-17 03:35:22 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./Pages/Login";
+import { Signup } from "./Pages/Signup";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,11 @@
+export const Signup = () => {
+  return (
+    <>
+      <h1>Signup</h1>
+      <form>
+        <input />
+        <input />
+      </form>
+    </>
+  );
+};
