d7afbe9fa8f1008694666cb37d4f99aeaaa45284
신희성 <vvwv3092@gmail.com>
2024-02-17 03:36:48 +0900

    Add login, signup page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const Login = () => {
   return (
     <>
       <h1>Login</h1>
+      <form>
+        <input placeholder="email" type="email" required />
+        <input placeholder="password" type="password" required />
+        <button type="submit">Login</button>
+      </form>
+      <Link to="/signup">signup</Link>
     </>
   );
 };
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 export const Signup = () => {
   return (
     <>
       <h1>Signup</h1>
       <form>
-        <input />
-        <input />
+        <input placeholder="email" type="email" required />
+        <input placeholder="name" required />
+        <input placeholder="password" type="password" required />
+        <button type="submit">Login</button>
       </form>
+      <Link to="/login">login</Link>
     </>
   );
 };
