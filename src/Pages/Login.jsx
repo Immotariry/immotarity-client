6510a20e0cb0a277a8eaf8d5dbd8b94f4f971ea2
신희성 <vvwv3092@gmail.com>
2024-02-17 04:06:44 +0900

    Add login, signup logic

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  const [email, setEamil] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    return axios.post(
+      "http://localhost:8080/immotarity/api/user/login",
+      {
+        email: email,
+        password: password,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <form>
-        <input placeholder="email" type="email" required />
-        <input placeholder="password" type="password" required />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          login();
+        }}
+      >
+        <input
+          placeholder="email"
+          type="email"
+          value={email}
+          required
+          onChange={(e) => setEamil(e.currentTarget.value)}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
         <button type="submit">Login</button>
       </form>
       <Link to="/signup">signup</Link>
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,16 +1,52 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signup = () => {
+    return axios.post("http://localhost:8080/immotarity/api/user/signup", {
+      email: email,
+      username: name,
+      password: password,
+    });
+  };
+
   return (
     <>
       <h1>Signup</h1>
-      <form>
-        <input placeholder="email" type="email" required />
-        <input placeholder="name" required />
-        <input placeholder="password" type="password" required />
-        <button type="submit">Login</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          signup();
+        }}
+      >
+        <input
+          placeholder="email"
+          type="email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.currentTarget.value)}
+        />
+        <input
+          placeholder="name"
+          value={name}
+          required
+          onChange={(e) => setName(e.currentTarget.value)}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+        <button type="submit">Signup</button>
       </form>
-      <Link to="/login">login</Link>
+      <Link to="/">login</Link>
     </>
   );
 };
